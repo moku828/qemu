7bc457b989235a6f4508f26fbc95c271dcf84f3f
Masayuki Okumura <basashi@sannet.ne.jp>
2020-04-18 23:23:50 +0900

    Add comment for regbank two-dimensional array

--- target/sh4/cpu.h
+++ target/sh4/cpu.h
@@ -175,7 +175,7 @@ typedef struct CPUSH4State {
     uint32_t mach;		/* multiply and accumulate high */
     uint32_t macl;		/* multiply and accumulate low */
     uint32_t pr;		/* procedure register */
-    uint32_t regbank[15][20];   /* register bank */
+    uint32_t regbank[15][20];   /* register bank [bank number][entry number] */
     uint32_t bn_max;		/* max. bank number */
     uint32_t ibnr;		/* bank number register */
     uint32_t ibcr;		/* bank control register */
