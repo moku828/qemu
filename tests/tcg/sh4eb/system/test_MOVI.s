18e937aab5cff89d6aa4464cc8ad8774ce9564ac
Masayuki Okumura <basashi@sannet.ne.jp>
2020-08-14 03:31:45 +0900

    LP-43 Test MOVe constant value

--- tests/tcg/sh4eb/system/test_MOVI.s
+++ tests/tcg/sh4eb/system/test_MOVI.s
@@ -9,8 +9,8 @@ test:
 	/* t2.stepi() */
 	/* t2.expect_registers({"r0":"0x7f"}) */
 	
-	MOV     #0x80, R1
+	MOV     #0x80, R0
 	/* t2.stepi() */
-	/* t2.expect_registers({"r1":"0xffffff80"}) */
+	/* t2.expect_registers({"r0":"0xffffff80"}) */
 	
 	/* test_scenario end */
