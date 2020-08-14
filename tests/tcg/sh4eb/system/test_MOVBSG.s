fadf8fda9aae4295ecdd2325a482e15ff992b12a
Masayuki Okumura <basashi@sannet.ne.jp>
2020-08-14 13:04:02 +0900

    LP-45 Test MOVe global data

---
+++ tests/tcg/sh4eb/system/test_MOVBSG.s
@@ -0,0 +1,37 @@
+.global test
+
+.section .text, "ax"
+
+test:
+	/* test_scenario start */
+	
+	MOV.L   immdata_0x0c000000, R1
+	/* t2.stepi() */
+	
+	LDC     R1, GBR
+	/* t2.stepi() */
+	
+	MOV     #0x7F, R0
+	/* t2.stepi() */
+	
+	MOV.B   R0, @(4, GBR)
+	/* t2.stepi() */
+	/* t2.expect_memories({"0x0c000004":"0x7f"}, "1") */
+	
+	MOV.L   immdata_0x0c000000, R1
+	/* t2.stepi() */
+	
+	LDC     R1, GBR
+	/* t2.stepi() */
+	
+	MOV     #0x80, R0
+	/* t2.stepi() */
+	
+	MOV.B   R0, @(4, GBR)
+	/* t2.stepi() */
+	/* t2.expect_memories({"0x0c000004":"0x80"}, "1") */
+	
+	/* test_scenario end */
+	.align  4
+immdata_0x0c000000:
+	.long   0x0C000000
